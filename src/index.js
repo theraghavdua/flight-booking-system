f897604bd78f9d3fc30d2ddf408e73b3212cc76d
theraghavdua <raghavdua130@gmail.com>
2025-03-13 23:45:28 +0530

    initial project setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { PORT } = require("./config");
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Successfully started the server on PORT : ${PORT}`);
+});
